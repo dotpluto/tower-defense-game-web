1cf8c3d8d780e322b730a0c73b2785b72cdaa63d
dotpluto <dotpluto@proton.me>
2025-01-05 10:56:47 +0100

    moving server dir out of source tree

--- build.sh
+++ build.sh
@@ -1,11 +1,35 @@
-#clean public folder
-rm -r ./public/*
+#options
+shopt -s nullglob #if glob doesn't match anything return nothing
+
+#sourcing environment variables
+if [ -e ./.env ]; then
+	source ./.env
+else
+	echo "You do not have a .env file set up for this environment. Aborting."
+	exit 1
+fi
+
+if [ -z ${DEV_WEBROOT} ]; then
+	echo "DEV_WEBROOT is not set. Aborting."
+	exit 1
+fi
+
+if [ ! -d ${DEV_WEBROOT} ]; then
+	echo "DEV_WEBROOT is not a valid dir. Aborting."
+	exit 1
+fi
+
+#clean webroot
+cleanup_list=${DEV_WEBROOT}*
+if [ -n $cleanup_list ]; then
+	rm -rv $cleanup_list
+fi
 
 #individual files
-cp ./index.html ./public/
-cp ./style.css ./public/
+cp ./*.html ${DEV_WEBROOT}
+cp ./*.css ${DEV_WEBROOT}
 
 #folders
-cp -r ./assets ./public/
+cp -r ./assets/ ${DEV_WEBROOT}
 
-tsc
+tsc --outDir ${DEV_WEBROOT}
